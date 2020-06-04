aed92bdb650ea5a78305f5a1f7a4c12636a104ae
David Alvarez C <daalvarez17@utpl.edu.ec>
2020-06-04 13:04:20 -0500

    prueba y error

--- repaso.java
+++ repaso.java
@@ -3,6 +3,11 @@ package PracticaPrueba;
 import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.*;
+/*
+*Autor:
+*@david Alvarez C
+*
+*/
 
 public class repaso {
     public static void main(String[] args) throws InterruptedException, ExecutionException {
